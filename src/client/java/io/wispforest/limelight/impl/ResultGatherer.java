d3e11d054f124d890afccb2a3599ff8f4c293ce1
Basique <basiqueevangelist@yandex.ru>
2024-09-01 23:43:48 +0300

    add checks to swallow and log exceptions while gathering results

--- src/client/java/io/wispforest/limelight/impl/ResultGatherer.java
+++ src/client/java/io/wispforest/limelight/impl/ResultGatherer.java
@@ -3,26 +3,43 @@ package io.wispforest.limelight.impl;
 import io.wispforest.limelight.api.entry.ResultEntry;
 import io.wispforest.limelight.api.entry.ResultGatherContext;
 import io.wispforest.limelight.api.extension.LimelightExtensions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.function.Consumer;
 
 public final class ResultGatherer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("Limelight/ResultGatherer");
+
     private ResultGatherer() {
 
     }
 
     public static void findResults(ResultGatherContext ctx, Consumer<ResultEntry> resultConsumer) {
         for (var extension : LimelightExtensions.enabledExtensions()) {
-            var gatherer = extension.checkExclusiveGatherer(ctx);
+            try {
+                var gatherer = extension.checkExclusiveGatherer(ctx);
 
-            if (gatherer != null) {
-                gatherer.gatherEntries(ctx, resultConsumer);
-                return;
+                if (gatherer != null) {
+                    try {
+                        gatherer.gatherEntries(ctx, resultConsumer);
+                    } catch (Exception e) {
+                        LOGGER.error("`{}`'s exclusive result gatherer failed", extension.id(), e);
+                    }
+
+                    return;
+                }
+            } catch (Exception e) {
+                LOGGER.error("`{}`'s checkExclusiveGatherer method failed", extension.id(), e);
             }
         }
 
         for (var extension : LimelightExtensions.enabledExtensions()) {
-            extension.gatherEntries(ctx, resultConsumer);
+            try {
+                extension.gatherEntries(ctx, resultConsumer);
+            } catch (Exception e) {
+                LOGGER.error("`{}`'s result gatherer failed", extension.id(), e);
+            }
         }
     }
 }
