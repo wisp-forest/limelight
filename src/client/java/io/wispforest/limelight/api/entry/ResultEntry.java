6b4c5e4d65e996f54157ac023180757f45a41196
Jakob <minenash@protonmail.com>
2024-09-10 15:19:26 -0400

    concept for prefix system

--- src/client/java/io/wispforest/limelight/api/entry/ResultEntry.java
+++ src/client/java/io/wispforest/limelight/api/entry/ResultEntry.java
@@ -3,6 +3,8 @@ package io.wispforest.limelight.api.entry;
 import io.wispforest.limelight.api.extension.LimelightExtension;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Language;
+import net.minecraft.util.Util;
 
 /**
  * Represents a result entry in the Limelight GUI.
@@ -24,4 +26,15 @@ public sealed interface ResultEntry permits InvokeResultEntry, SetSearchTextEntr
      * @return the text of this entry
      */
     Text text();
+
+    /**
+     * @return the prefix used to identify the extension in the search result.
+     * @apiNote By default, it'll use the translation if it exists, if it doesn't, it'll use the extension's name
+     * */
+    default Text prefix() {
+        String key = Util.createTranslationKey("limelightExtension", extension().id()) + ".prefix";
+        if (Language.getInstance().hasTranslation(key))
+            return Text.translatable(key);
+        return extension().name();
+    }
 }
